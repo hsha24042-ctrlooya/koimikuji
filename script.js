77c50418b31b277a6ac22d9440a2c7e6d5d3258f
ooya <ooya@hsha.com>
2026-05-01 12:12:09 +0900

    おみくじの結果を追加

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-const fortunes=[
-    "大吉💛素敵な出会いがあるかも！",
-    "中吉少し勇気を出すとよいことが起きる",
-    "小吉今日はゆっくり過ごそう",
-    "凶無理せず慎重に行動しよう"
+const fortunes = [
+    "超大吉　今日の君は無敵だ",
+    "大吉　💛素敵な出会いがあるかも！",
+    "中吉　少し勇気を出すとよいことが起きる",
+    "小吉　今日はゆっくり過ごそう",
+    "凶　無理せず慎重に行動しよう"
 ];
 
 //ボタンと表示エリアを取得
-const button=document.getElementById("os")
-const result=document.getElementById("result")
+const button=document.getElementById("omikuji-bt");
+const result=document.getElementById("result");
 
 //ボタンがクリックされた時の処理
-button.addEventListener("click",function(){
+button.addEventListener("click",function() {
     //ランダムな数字を作る
-    const randomNumber=Math.floor(Math.random()*foetunes.lenght);
+    const randomNumber = Math.floor(Math.random() * fortunes.lenght);
    
     //結果を表示
-    result.textContent=fortunes[(randomNumber)];
+    result.textContent = fortunes[(randomNumber)];
 });
